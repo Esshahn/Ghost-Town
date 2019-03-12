22c2a6d56ddadd9264221d0e8246686325ccaf0e
Jan Wassermann <jan.wassermann@jansalleine.com>
2019-03-12 14:39:03 +0100

    C64 und C128 Directories wie plus4 Directory

--- C64/test/testscreen.asm
+++
@@ -1,5 +0,0 @@
-                    *= 0x0400
-                    ;!bin "screen01.prg",1000,2
-                    !bin "screen02.prg",1000,2
-                    *= 0x0810
-code_start:         jmp *
